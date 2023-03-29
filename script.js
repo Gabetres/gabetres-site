448fdaa25da1086dca813a5eafa5197c5f61fca2
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-03-29 01:40:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ mode.addEventListener('click', () => {
     if(mode.classList.contains('fa-moon')){
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
-        document.getElementById("changeimage").src="img/gabrielblue.png"
+        document.getElementById("changeimage").src="img/gabrielblue.png";
+
 
         return;
     }
