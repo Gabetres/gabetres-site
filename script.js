dd31dda116a62ea10eee6b3d391cf299b49cb5ad
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-03-29 00:01:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const html = document.querySelector('html');
+const mode = document.querySelector('#mode-icon');
+
+
+mode.addEventListener('click', function(){
+html.classList.toggle('dark-mode');
+});
+
+mode.addEventListener('click', () => {
+    if(mode.classList.contains('fa-moon')){
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+        document.getElementById("changeimage").src="img/gabrielblue.jpg"
+
+        return;
+    }
+        mode.classList.add('fa-moon');
+        mode.classList.remove('fa-sun');
+        document.getElementById("changeimage").src="img/gabrielred.png"
+    }
+);
